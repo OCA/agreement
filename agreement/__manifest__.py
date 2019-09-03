284a3f2a3d3faf26690e4505000520b0d8848d7e
黎伟杰 <674416404@qq.com>
2019-09-03 11:36:40 +0000

    [IMP] move some notions from agreement_legal to agreement
    we move the is_template field definition and the agreement.type model from
    the agreement_legal module to the agreement module.
    
    The fields are not displayed by default, unless the feature is enabled through a
    technical feature group, this is configurable in the agreement_sale module (because agreement
    in itself has no UI, and agreement_legal enables the feature by default)

--- agreement/__manifest__.py
+++ agreement/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Agreement',
     'summary': "Adds an agreement object",
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Contract',
     'author': 'Akretion, '
               'Yves Goldberg (Ygol Internetwork), '
@@ -16,6 +16,7 @@
         'security/ir.model.access.csv',
         'security/agreement_security.xml',
         'views/agreement.xml',
+        'views/agreement_type.xml',
         ],
     'demo': ['demo/demo.xml'],
     'development_status': 'Beta',
