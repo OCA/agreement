4447820d2e7db77331ca85d1cd4414c0cfe5d9fd
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2021-05-04 11:38:03 -0600

    [14.0][MIG] agreement_legal (Version 12.0 to 14.0)
    [MIG] Black & Cleanup
    
    [IMP] Reviewer Requested Changes
    
    [IMP] Black
    
    [IMP] Reviewer Suggestion
    
    [FIX] Tests
    
    [FIX] Multiple Issues
    
    - Added 'is_old_version' field to better display old versions. Before these were marked as inactive therefore not showing in the agreement.
    - 'is_template' field now copies when making a template revision or copying a template.
    - New Versions now retain original create date.
    - Moved version/revision and created on/by fields out of footer to top in order to have the save buttons when creating children.
    - Moved demo data out of data folder into the demo.xml
    
    [FIX] demo data
    
    [FIX] Revert back to only migration
    
    [FIX] Report dynamic field rendering
    
    [FIX] Test

--- agreement_legal/__manifest__.py
+++ agreement_legal/__manifest__.py
@@ -11,14 +11,13 @@
     "website": "https://github.com/OCA/contract",
     "category": "Partner",
     "license": "AGPL-3",
-    "version": "12.0.2.0.1",
+    "version": "14.0.1.0.1",
     "depends": ["contacts", "agreement", "product"],
     "data": [
         "data/ir_sequence.xml",
         "data/module_category.xml",
         "data/agreement_stage.xml",
         "data/agreement_type.xml",
-        "data/demo.xml",
         "security/res_groups.xml",
         "security/ir.model.access.csv",
         "report/agreement.xml",
@@ -36,14 +35,8 @@
         "views/agreement.xml",
         "views/menu.xml",
     ],
-    "demo": [
-        "demo/demo.xml"
-    ],
-    "post_init_hook": "post_init_agreement_legal",
+    "demo": ["demo/demo.xml"],
     "application": True,
     "development_status": "Beta",
-    "maintainers": [
-        "max3903",
-        "ygol"
-    ],
+    "maintainers": ["max3903", "ygol"],
 }
