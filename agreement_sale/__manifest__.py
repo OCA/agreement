dd7c1b8120daf3760f57f54a02c8842f7303c8c7
vibhascs <vibha.k.serpentcs@gmail.com>
2023-07-03 11:37:11 +0530

    [16.0][MIG]agreement_sale

--- agreement_sale/__manifest__.py
+++ agreement_sale/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "16.0.1.0.0",
     "category": "Contract",
     "author": "Akretion, " "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/contract",
+    "website": "https://github.com/OCA/agreement",
     "license": "AGPL-3",
     "depends": ["sale_management", "agreement"],
     "data": [
